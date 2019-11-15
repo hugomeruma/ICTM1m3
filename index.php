00713cd068b38d96452bdf5a69d384a63a305f5c
Alexander Roos <alexander@famroos.nu>
2019-11-15 14:19:47 +0100

    Deleting from useless functionality.

--- index.php
+++ index.php
@@ -1,23 +1,6 @@
 <?php
 require 'helpers.php';
 
-// Load routes
-$routes = [];
-
-// Check if route exists in routes.php
-if ($_SERVER["REQUEST_METHOD"] == "POST" AND array_key_exists(self::getUri(), $routes["post"])){
-    // Get function and functionFile by array key
-    list($function, $functionFile) = explode("@", $routes["post"][self::getUri()]);
-}
-elseif (array_key_exists(self::getUri(), $routes["get"])){
-    // Get controller and method by array key
-    list($function, $functionFile) = explode("@", $routes["get"][self::getUri()]);
-}
-else{
-    // If now route defined go to 404 page
-    return redirect("404");
-}
-
 // Require functions file
 require ("functies/{$functionFile}");
 
