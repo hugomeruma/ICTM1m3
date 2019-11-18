18ed10652496835952a343800d713959835bd391
Hugo Meruma <hugo@handmadewebdesign.nl>
2019-11-18 10:44:26 +0100

    Change project structure

--- index.php
+++ index.php
@@ -1,26 +1,7 @@
 <?php
-require 'helpers.php';
+require "functies/contentFuncties.php";
+require "parts/head.php";
 
-// Load routes
-$routes = [];
-require 'routes.php';
+// body
 
-// Check if route exists in routes.php
-if ($_SERVER["REQUEST_METHOD"] == "POST" AND array_key_exists(self::getUri(), $routes["post"])){
-    // Get function and functionFile by array key
-    list($function, $functionFile) = explode("@", $routes["post"][self::getUri()]);
-}
-elseif (array_key_exists(self::getUri(), $routes["get"])){
-    // Get controller and method by array key
-    list($function, $functionFile) = explode("@", $routes["get"][self::getUri()]);
-}
-else{
-    // If now route defined go to 404 page
-    return redirect("404");
-}
-
-// Require functions file
-require ("functies/{$functionFile}");
-
-// Load function from functions file
-$function();
+require "parts/footer.php";
