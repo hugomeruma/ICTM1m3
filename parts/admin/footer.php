e1b51b66943e2f9763e6f5febb92ca1e9c217db8
Hugo Meruma <hugo@handmadewebdesign.nl>
2019-11-18 12:02:23 +0100

    Add admin parts and menu

---
+++ parts/admin/footer.php
@@ -0,0 +1,7 @@
+<body>
+<!-- Scripts -->
+<script src="../../assets/js/jquery.js"></script>
+<script src="../../assets/js/popper.js"></script>
+<script src="../../assets/js/bootstrap.min.js"></script>
+</body>
+</html>
