8661f7e1ec4b323108487644d2a148928e44cfaf
Alexander Roos <alexander@famroos.nu>
2019-11-27 14:33:26 +0100

    Edited something for the search query

--- product/parts/infoTabel.php
+++ product/parts/infoTabel.php
@@ -2,11 +2,23 @@
 
 <?php
 foreach ($product as $key => $value) {
-    if ("" != ($value)) {
-        unset($product[$value]);
+    if ("" == ($value) or "0" == ($value)) {
+        unset($product[$key]);
     }
 }
-
+foreach ($product as $key => $value) :
+    ?>
+    <tr>
+        <th scope="row"><?= $key ?></th>
+        <td><?= $value ?></td>
+    </tr>
+<?php
+endforeach;
 ?>
+
+<tr>
+    <th scope="row"><?= $key ?></th>
+    <td><?= $product ?></td>
+</tr>
 
 <!-- </table> -->
