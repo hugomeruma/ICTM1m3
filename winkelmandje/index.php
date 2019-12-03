3de987b84212b30ecb4e4a9772317325a89e9993
moeen <s1128566@student.windesheim.nl>
2019-12-03 14:24:58 +0100

    veder gewerkt aan winkelmandje ((teller))

--- winkelmandje/index.php
+++ winkelmandje/index.php
@@ -5,26 +5,46 @@ require __dir__ . "/../functies/helpers.php";
 require __dir__ . "/../functies/contentFuncties.php";
 require __dir__ . "/../parts/head.php";
 
+?>
+
+<!--    <div class="my-5">-->
+<!--        --><?php
+//        $key = "StockItemID1";
+//        if (strpos($key, 'StockItemID') !== false) {
+//            echo "$key";
+//            $key = str_replace('StockItemID', '', $key);
+////            $_SESSION['producten'][$key] = $value;
+//            echo "<h1>$key</h1>";
+//        }
+//        ?>
+<!--    </div>-->
+
+<?php
+if (isset($_POST['opslaan'])) {
+    echo "test 1";
+    foreach ($_POST as $key => $value) {
+        if (strpos($key, 'StockItemID') !== false) {
+            echo "$key";
+            $key = str_replace('StockItemID', '', $key);
+            $_SESSION['producten'][$key] = $value;
+        }
+    }
+//    redirect('winkelmandje/');
+}
 
 if (isset($_POST['StockItemID'])) {
-
+    echo $_POST['StockItemID'];
+    print_r($_SESSION['producten'][$_POST['StockItemID']]);
     if (isset($_SESSION['producten'][$_POST['StockItemID']])) {
         $i = $_SESSION['producten'][$_POST['StockItemID']] + 1;
     } else {
         $i = 1;
     }
     $_SESSION['producten'][$_POST['StockItemID']] = $i;
+
+//    redirect('winkelmandje/');
 }
-//dd($_SESSION);
-if (isset($_POST['opslaan'])) {
-    foreach ($_POST as $key => $value) {
-        if (strpos($key, 'product') !== false) {
-            str_replace('product', '', $key);
-            $_SESSION['producten'][$key] = $value;
-        }
-    }
-    redirect('winkelmandje/');
-}
+
 
 $winkelwagen = array();
 if (isset($_SESSION['producten'])) {
@@ -33,48 +53,41 @@ if (isset($_SESSION['producten'])) {
 
 $totaalorder = 0;
 
-//foreach($winkelwagen as $value)
-//{
-//
-//    //Naam item
-//    echo $value['StockItemName'];
-//    echo "<br>";
-//    //Aantallen item
-//    echo $_SESSION['producten'][$value['StockItemID']];
-////    echo "<br>";
-////    //Retail prijs
-////    echo $value['RecommendedRetailPrice'];
-////    echo "<br>";
-////    //Aantallen item * de retailprijs = totaal prijs per item
-////    echo "Subtotaal =".$_SESSION['producten'][$value['StockItemID']] * $value['RecommendedRetailPrice'];
-////    echo "<br>";
-////
-////    $totaalorder += $_SESSION['producten'][$value['StockItemID']] * $value['RecommendedRetailPrice'];
-////}
-
-// SELECT * FROM `stockitems` WHERE StockItemId  IN (array_keys($_SESSION['producten']))
-
-
 ?>
-<div class="container">
-    <form method="post">
-    <?php
-    foreach ($winkelwagen as $product):
+    <div class="container">
+        <form method="post">
+            <?php
+            foreach ($winkelwagen as $product):
+
+
+                ?>
+                <div class="container row product_kaart my-4">
+                    <?php require "parts/item.php" ?>
+
+
+                </div>
+            <?php
+
+
+            endforeach;
+
+
+            ?>
+
+            <button type="submit" value="opslaan" class="opslaan_winkelmandje btn btn-primary" name="opslaan">
+                Winkelmandje opslaan
+            </button>
+        </form>
+    </div>
+
+    <div class="my-5">
+        <?php
+        print_r($_SESSION);
+        echo "<br>";
+        print_r($_POST)
         ?>
-        <div class="container row product_kaart my-4">
-            <?php require "parts/item.php" ?>
-
-
-        </div>
-    <?php
-    endforeach;
-    ?>
-<button type="submit" name="opslaan">test</button>
-    </form>
-</div>
-
+    </div>
 <?php
-
 
 
 require __dir__ . "/../parts/footer.php";
--- winkelmandje/parts/item.php
+++ winkelmandje/parts/item.php
@@ -22,20 +22,34 @@
     </div>
 </div>
 <?php
-$teller= $_SESSION['producten'][$product['StockItemID']];
+$teller = $_SESSION['producten'][$product['StockItemID']];
 
 $prijs_per_stuk = price($product['UnitPrice'], $product['TaxRate']);
 $subTotaal = $prijs_per_stuk * $teller;
+
+
 ?>
 <div class="col-3 product_prijs" style="text-align: right">
-    <h5>Sub-Totaal: € <?= $subTotaal ?>,-</h5>
-    <h6>Prijs per stuk: € <?= $prijs_per_stuk ?>,-<h6>
-    <!--    <toevoegen aan winkelmandje><button> met icon /button>-->
-            <div class="btn-group" role="group" aria-label="Basic example">
-                <input type="number" value="<?= $teller ?>" name="product<?= $product['StockItemID'] ?>" class="form">
-            </div>
-    <button type="button" class="btn btn-primary">verwijder</button>
+    <h6>Prijs per stuk: € <?= $prijs_per_stuk ?>,-
+        <h6>
+            <h5>Sub-Totaal: € <?= $subTotaal ?>,-</h5>
 
+            <!--    <toevoegen aan winkelmandje><button> met icon /button>-->
+                <div class="form-group form-inline winkelmandje_form_group" role="group" aria-label="Basic example"><p></p>
+                    <form method="post">
+                        <button type="Submit" name="StockItemID<?= $product['StockItemID'] ?>" value="0"
+                                class="btn btn-primary button_winkelmandje_verw">verwijder</button>
+                        <input type="hidden" name="opslaan" value="opslaan"
+                               class="form teller_form">
+                    </form>
+                    <form method="post">
+                    <input type="number" value="<?= $teller ?>" name="StockItemID<?= $product['StockItemID'] ?>"
+                           class="form teller_form">
+                    <input type="hidden" name="opslaan" value="opslaan"
+                           class="form teller_form">
+
+
+                </div>
 
 </div>
 
