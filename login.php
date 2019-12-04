4714ecd8f3f8e3bcf9aca4a826bcecd420d8bafa
Alexander Roos <alexander@famroos.nu>
2019-12-04 12:01:51 +0100

    Updated the shopingcard, fixed buttons ande deleting

--- login.php
+++ login.php
@@ -5,6 +5,7 @@ require __DIR__ . "/functies/helpers.php";
 require __DIR__ . "/functies/contentFuncties.php";
 require __DIR__ . "/functies/account.php";
 require __DIR__ . "/parts/head.php";
+require __DIR__ . "/parts/navbar.php";
 
 
 
