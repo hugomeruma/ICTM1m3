b7220caac4485785f0ca36de17bbd0c5f3b77188
Hugo Meruma <hugo@handmadewebdesign.nl>
2019-12-04 12:32:46 +0100

    Slechte functie naam aangepast

--- admin/wachtwoord-wijzigen.php
+++ admin/wachtwoord-wijzigen.php
@@ -8,7 +8,7 @@ require __DIR__ . "/../parts/admin/head.php";
 require __DIR__ . "/../parts/admin/menu.php";
 
 if (isset($_POST['submit'])) {
-    if (wijzigWachtwoordValidatie($_SESSION['email'], $_POST['huidigWachtwoord'], $_POST['huidigWachtwoord'], $_POST['nieuwWachtwoordHerhaling'])) {
+    if (wijzigWachtwoord($_SESSION['email'], $_POST['huidigWachtwoord'], $_POST['huidigWachtwoord'], $_POST['nieuwWachtwoordHerhaling'])) {
         if (wijzigWachtwoord($_SESSION['id'], $_POST['huidigWachtwoord'])) {
             $_SESSION['alert']['success']['title'] = 'Succes!';
             $_SESSION['alert']['success']['message'] = 'Het wachtwoord is aangepast.';
