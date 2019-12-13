db7e9eceabdf36e262ff593c08d3b1a8ac05f410
Jestin Wegert <S1131724@student.windesheim.nl>
2019-12-13 14:38:24 +0100

    niets belangrijks

--- wachtwoordWijzigen.php
+++ wachtwoordWijzigen.php
@@ -36,7 +36,7 @@ if (isset($message)) {
 ?>
 <form method="post">
     <div class="col-lg-6 form-group">
-        <label for="">email*</label>
+        <label for="">e<Email*</label>
         <input type="text" class="form-control" id="" name="email"
                value="<?= $account['email'] ?? '' ?>" readonly required>
     </div>
